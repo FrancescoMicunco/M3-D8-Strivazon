ab9e534504aec57004d72c3ecb9e25504baa3c73
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-03 20:44:45 +0100

    front page update complete

--- backoffice.js
+++ backoffice.js
@@ -1,49 +1,26 @@
-/* const getData = async() => {
-    let data = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
-        headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzU5NDU4MDksImV4cCI6MTYzNzE1NTQwOX0.8-JnqeWjE2RLRn6wo9eXe6lfTacIEpgdPvytb9NRr0I"
-        }
-    })
-    try {
-        if (data.ok) {
-            console.log(data)
-            let products = await data.json()
-            return products;
-        } else {
-            alert("fetched din't appen")
-        }
-    } catch (err) { console.log("error") }
-
-} */
-
 window.onload = async() => {
+        getData()
+        const form = document.querySelector("form")
+        form.onsubmit = async function(e) {
+            let name = document.getElementById("name").value
+            let description = document.getElementById("description").value
+            let brand = document.getElementById("brand").value
+            let imageUrl = document.getElementById("image").value
+            let price = document.getElementById("price").value
+            const product = {
+                name,
+                description,
+                brand,
+                imageUrl,
+                price
+            }
+            e.preventDefault()
+            const newProduct = await postData(product)
 
-
-    const form = document.querySelector("form")
-
-    form.onsubmit = async function(e) {
-
-        let name = document.getElementById("name").value
-        let description = document.getElementById("description").value
-        let brand = document.getElementById("brand").value
-        let imageUrl = document.getElementById("image").value
-        let price = document.getElementById("price").value
-
-
-        const product = {
-            name,
-            description,
-            brand,
-            imageUrl,
-            price
         }
-        e.preventDefault()
-        const newProduct = await postData(product)
+
     }
-}
-
-
-// post product
+    // post product
 
 const postData = async(product) => {
     let dataPost = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
@@ -53,10 +30,34 @@ const postData = async(product) => {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzU5NDU4MDksImV4cCI6MTYzNzE1NTQwOX0.8-JnqeWjE2RLRn6wo9eXe6lfTacIEpgdPvytb9NRr0I"
         },
         body: JSON.stringify(product),
-
     })
     let newProduct = await dataPost.json()
 
 }
 
-// form for product
+// get product
+const getData = async(e) => {
+    let data = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzU5NDU4MDksImV4cCI6MTYzNzE1NTQwOX0.8-JnqeWjE2RLRn6wo9eXe6lfTacIEpgdPvytb9NRr0I"
+        }
+    })
+    if (data.ok) {
+        console.log(data)
+        let products = await data.json()
+        let row = document.getElementById("cards")
+        products.forEach(element => {
+            row.innerHTML += `<p>${element.name}</p> 
+            <p class="card-text ml-4">${element.description}</p>
+            <p class="card-text ml-4">${element.description}</p>
+            <p class="card-text ml-4">${element.brand}</p>
+            <p class="card-text ml-4">${element.image}</p>
+            <p class="card-text ml-4">${element.price}</p>
+            `
+        });
+    } else {
+        alert("fetched din't appen")
+    }
+}
