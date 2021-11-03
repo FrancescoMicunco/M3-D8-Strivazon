97b01085511995a73c20908844d73948aa9d5218
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-03 18:29:29 +0100

    post request

--- backoffice.js
+++ backoffice.js
@@ -1,26 +1,3 @@
-let pName = document.getElementById("product")
-let description = document.getElementById("description")
-let brand = document.getElementById("brand")
-let image = document.getElementById("image")
-let price = document.getElementById("price")
-let product = new Object()
-
-
-function postNewData() {
-    console.log(pName.value)
-}
-
-/* function postNewData() {
-    product.name = pName.value;
-    product.description = description.value;
-    product.brand = brand.value;
-    product.image = image.value;
-    product.price = price.value;
-    console.log(product)
-}
- */
-
-
 /* const getData = async() => {
     let data = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
         headers: {
@@ -40,31 +17,45 @@ function postNewData() {
 } */
 
 window.onload = async() => {
-    const product = await postData()
-    console.log(product)
 
+
+    const form = document.querySelector("form")
+
+    form.onsubmit = async function(e) {
+
+        let name = document.getElementById("name").value
+        let description = document.getElementById("description").value
+        let brand = document.getElementById("brand").value
+        let imageUrl = document.getElementById("image").value
+        let price = document.getElementById("price").value
+
+
+        const product = {
+            name,
+            description,
+            brand,
+            imageUrl,
+            price
+        }
+        e.preventDefault()
+        const newProduct = await postData(product)
+    }
 }
 
 
 // post product
 
-const postData = async() => {
+const postData = async(product) => {
     let dataPost = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzU5NDU4MDksImV4cCI6MTYzNzE1NTQwOX0.8-JnqeWjE2RLRn6wo9eXe6lfTacIEpgdPvytb9NRr0I"
         },
-        body: JSON.stringify({
-            name: `Alphabet`,
-            description: "the best phone of your life",
-            brand: "Alphabet",
-            imageUrl: "https://www.unieuro.it/online/iPhone/iPhone-12-128GB---Verde-pidAPLIPHONE12128GR?gclsrc=aw.ds&gclid=Cj0KCQjw5oiMBhDtARIsAJi0qk2cWZdeXlOos1a3eeeOU4XQrvtyLyFk9X6bDcFDaOPuJHV3CjCF2Z0aAqPfEALw_wcB&gclsrc=aw.ds",
-            price: 100
-        }),
+        body: JSON.stringify(product),
 
     })
-    let products = await dataPost.json()
+    let newProduct = await dataPost.json()
 
 }
 
