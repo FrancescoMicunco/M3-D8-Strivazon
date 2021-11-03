2c3960b3787de6f056c2d5d3152e1dc84ff22c1c
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-03 15:03:44 +0100

    fetch ok

--- backoffice.js
+++ backoffice.js
@@ -1,1 +1,23 @@
-console.log("backoffice works")
+const getData = async() => {
+    let data = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzU5NDU4MDksImV4cCI6MTYzNzE1NTQwOX0.8-JnqeWjE2RLRn6wo9eXe6lfTacIEpgdPvytb9NRr0I"
+        }
+    })
+
+
+    try {
+        if (data.ok) {
+            console.log(data)
+            let products = await data.json()
+            return products;
+        } else {
+            alert("fetched din't appen")
+        }
+    } catch (err) { console.log("error") }
+
+}
+
+window.onload = async() => {
+    const product = await getData()
+}
